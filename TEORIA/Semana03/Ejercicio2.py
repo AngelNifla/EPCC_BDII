c4a1147f1e72c186e5a7ca14dec3f7b9994f2c62
AngelNifla <mnifla@unsa.edu.pe>
2022-05-04 13:59:43 -0500

    Código de los ejercicios en python

---
+++ TEORIA/Semana03/Ejercicio2.py
@@ -0,0 +1,42 @@
+#Movie_Ratings: Es una tabla de Películas que fueron evaluadas por usuarios, dándoles un puntaje del 1 al 5.
+#1)¿Cuántas películas fueron calificadas por Thomas?
+#2)¿Cuántas películas fueron calificadas por Matt?
+from distutils.log import info
+import mysql.connector
+from mysql.connector import Error
+
+def cuantas_peliculas(cursor):
+    registro = cursor.fetchone()    #para obtener una respuesta usamos el cursor y fetchone, que recibe una sola respuesta de la consultas.
+    print("-> Estas conectado a la bd: ", registro)
+    #cursor.execute("SELECT * FROM movie_ratings.usuario_califica_peliculas WHERE nombre_usuario='Thomas';")#Se hace la consulta a Mysql, con el comando para buscar todas las calificaciones que hizo Thomas
+    cursor.execute("SELECT * FROM movie_ratings.usuario_califica_peliculas WHERE nombre_usuario='Matt';")#Se hace la consulta a Mysql, con el comando para buscar todas las calificaciones que hizo Matt
+    resultados =cursor.fetchall()   #Se obtiene los resultados en una Tupla[num_de_califiaciones][nombre,pelicula,califiacion].
+    num_peliculas = len(resultados) #Se guarda en una variable el tamaño de la tupla obtenida
+    return num_peliculas#y se retorna.
+        
+
+
+try:    #Si todo sale bien
+    conexion = mysql.connector.connect(         #En una variable obtedremos la conexion, entonces
+        host='localhost',       #le enviamos los datos del server.
+        port=3306,
+        user='root',
+        password='1234',
+        db='movie_ratings'
+    )
+
+    if conexion.is_connected():     #Condicional para verificar si nos conectamos a la bd.
+        print("-> Conexion exitosa a la BD.")
+        cursor = conexion.cursor()  #Objeto que actua como conexion para hacer lecturas, inserciones, actualizaciones y eliminaciones.
+        cursor.execute("SELECT database();") #una vez establecida la conexion a la bd y el cursor, con execute nops permitira hacer una sentencia en la bd
+        registro = cursor.fetchone()    #para obtener una respuesta usamos el cursor y fetchone, que recibe una sola respuesta de la consultas.
+        print("-> Estas conectado a la bd: ", registro)
+        #print("-> Thomas califico: ",cuantas_peliculas(cursor)," Peliculas.")   #Se llama a la funcion que calculara el numero de peliculas de thomas.
+        print("-> Matt califico: ",cuantas_peliculas(cursor)," Peliculas.")   #Se llama a la funcion que calculara el numero de peliculas de Matt.
+    #Si algo falla en todo este bloque
+except Error as ex: #obtenemos el error en una varible
+    print("-> Error durante la conexion: ", ex) #e informamos cual es el error
+finally:        #Para finalizar la ejecucion cerramos la conexion con la bd.
+    if conexion.is_connected():
+        conexion.close()#y se cierra la conexion con la BD
+        print("-> La conexion a la Bd ha finalizado.")
---
+++ TEORIA/Semana03/Ejercicio4.py
@@ -0,0 +1,56 @@
+#Movie_Ratings: Es una tabla de Películas que fueron evaluadas por usuarios, dándoles un puntaje del 1 al 5.
+#3)¿Qué película fue la menor calificada por los usuarios?, ¿cuántos usuarios la calificaron?
+#   Disgregue por número de usuarios que la calificaron con un determinado puntaje.
+
+from asyncio.windows_events import NULL
+from distutils.log import info
+import mysql.connector
+from mysql.connector import Error
+
+def pelicula_menor(cursor): #funcion que nos devolvera la pelicula mas califiacada.
+    registro = cursor.fetchone()    #para obtener una respuesta usamos el cursor y fetchone, que recibe una sola respuesta de la consultas.
+    cursor.execute("SELECT * FROM usuario_califica_peliculas;")#Se hace la consulta a Mysql, con el comando para buscar todas las calificaciones.
+    resultados =cursor.fetchall()   #Se obtiene los resultados en una Tupla[num_de_califiaciones][nombre,pelicula,califiacion].
+    cursor.execute("SELECT * FROM peliculas;")#Se hace la consulta a Mysql, con el comando para buscar todos loas nombres de las peliculas.
+    name_peliculas =cursor.fetchall()   #Se obtiene los resultados en una Tupla[num_peliculas].
+    menor = 26   #variable para el numero con menor
+    cont = 0    #variable contador de califiaciones
+    peli_menor = NULL   #variable para almacenar el numero de tupla de peli con menor califiacaiones
+    for pelicula in name_peliculas:    #bucle para comparar cada peli pelicula=[(una_peli,),()...]
+        for linea in resultados:    #bucle para comparar cada noombre de peli calificada
+            if linea[1] == pelicula[0]    :   #Si el mobre de la peli con el de la peli califiacada son iguales #pelicula[0]=una_peli
+                cont = cont+1   #se va contando mas 1
+        if cont < menor:    #al finalizar el bucle de calificaciones, si el contador es menor que el contador de la comparacion anterior
+            menor = cont    #el contador actual se guarda en la variable menor,
+            peli_menor = pelicula[0] #se guarda el numero de la tupla de la peli que gano mas calificaciones que la anterior
+        cont = 0    #y volvemos al contador a 0.
+    print("-> La pelicula menos calificada por los usuarios es: ",peli_menor)#Se imprime la pelicula con mas califiaciones.
+    
+    return menor#y se retorna el nombre de la pelicula con menor calificaiones.
+        
+
+
+try:    #Si todo sale bien
+    conexion = mysql.connector.connect(         #En una variable obtedremos la conexion, entonces
+        host='localhost',       #le enviamos los datos del server.
+        port=3306,
+        user='root',
+        password='1234',
+        db='movie_ratings'
+    )
+
+    if conexion.is_connected():     #Condicional para verificar si nos conectamos a la bd.
+        print("-> Conexion exitosa a la BD.")
+        cursor = conexion.cursor()  #Objeto que actua como conexion para hacer lecturas, inserciones, actualizaciones y eliminaciones.
+        cursor.execute("SELECT database();") #una vez establecida la conexion a la bd y el cursor, con execute nops permitira hacer una sentencia en la bd
+        registro = cursor.fetchone()    #para obtener una respuesta usamos el cursor y fetchone, que recibe una sola respuesta de la consultas.
+        print("-> Estas conectado a la BD: ", registro[0])
+        print("-> El numero de usuarios que calificaron esta pelicula es: ",pelicula_menor(cursor), " usuarios")   #Se imprimira el valor que devuelva la funcion pelicula_menor().
+    
+    #Si algo falla en todo este bloque
+except Error as ex: #obtenemos el error en una varible
+    print("-> Error durante la conexion: ", ex) #e informamos cual es el error
+finally:        #Para finalizar la ejecucion cerramos la conexion con la bd.
+    if conexion.is_connected():
+        conexion.close()#y se cierra la conexion con la BD
+        print("-> La conexion a la Bd ha finalizado.")
